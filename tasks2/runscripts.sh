e68f0e8f689b0a53c629074a1e8f0c151f5a1d50
tocy1 <nwokotochukwu@gmail.com>
2019-05-18 14:48:50 +0100

    Update runscript.sh

--- tasks2/runscripts.sh
+++ tasks2/runscripts.sh
@@ -9,6 +9,6 @@ docker run -d --name my-running-haproxy my-haproxy --network appnetwork -p 80:80
 for i in {1..2}
 do 
   echo "creating and running default nginx container"
-  docker run --name mynginx"$i" -d nginx --network appnetwork
+  docker run --name nginx"$i" -d nginx --network appnetwork
   echo "done"
 done
